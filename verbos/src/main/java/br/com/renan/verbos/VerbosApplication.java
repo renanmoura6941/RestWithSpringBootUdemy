b5b4ac491150f710afa2730751bf2470e9c0ce0b
naneraruom <naneraruom@gmail.com>
2020-11-19 09:42:37 -0300

    verbos principais

---
+++ verbos/src/main/java/br/com/renan/verbos/VerbosApplication.java
@@ -0,0 +1,13 @@
+package br.com.renan.verbos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbosApplication.class, args);
+	}
+
+}
